5b1f90ff83e79ed47ceabf089b357c4861b4fe77
SMR <seyyedmortezarazavi76@gmail.com>
2022-03-29 23:58:42 +0430

    Enhances `MaskWindow`. + It will check every tile on window resize event, + To make it more dynamic, simpler, and easier to use TileMaskWindow will be renamed MaskWindow as a result of this change.

--- src/knight-pen/maskwindow.h
+++ src/knight-pen/maskwindow.h
@@ -4,164 +4,70 @@
 #include <QQuickPaintedItem>
 #include <QQuickWindow>
 #include <QQuickItem>
-#include <QPainter>
 #include <QBitmap>
 #include <QPixmap>
 #include <QRegion>
 #include <QRgb>
 
-using booleanMatrix = std::vector<std::vector<bool>>;
-
-class tileMaskWindow : public QQuickWindow {
+class maskWindow : public QQuickWindow {
     Q_OBJECT
-    Q_PROPERTY(int rows READ rows WRITE setRows NOTIFY rowsChanged)
-    Q_PROPERTY(int columns READ columns WRITE setColumns NOTIFY columnsChanged)
-    Q_PROPERTY(TransposeType transposed READ transposed WRITE setTransposed NOTIFY transposedChanged)
+    Q_PROPERTY(QSize sectorSize READ sector WRITE setSector NOTIFY sectorChanged)
 public:
-    enum TransposeType {
-        None,
-        PrimaryTranspose,
-        SecondaryTranspose,
-    };
-    Q_ENUM(TransposeType)
-
-    tileMaskWindow()
-        : mRows(1),
-          mColumns(1),
-          mTileSize(QSize(width() / mRows, height() / mColumns)),
-          mTransposed(TransposeType::None),
-          mMaskMatrix(booleanMatrix(1, std::vector<bool>(1, false))) {
+    maskWindow() : mSector(28, 28) {
         setFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint | Qt::Widget);
         setColor(Qt::transparent);
     }
 
-    void resizeEvent(QResizeEvent *ev) override {
-        mTileSize.setWidth(width()/mColumns);
-        mTileSize.setHeight(height()/mRows);
-
-        updateMask();
-        QQuickWindow::resizeEvent(ev);
+    void resizeEvent(QResizeEvent *event) override {
+        update();
+        QQuickWindow::resizeEvent(event);
     }
 
-    int rows() const { return mRows; }
-    int columns() const { return mColumns; }
-    TransposeType transposed() const { return mTransposed; }
-
-    void setRows(int newRows) {
-        if(mRows == newRows)
+    const QSize &sector() const { return mSector; }
+    void setSector(const QSize &newSectorSize) {
+        if(mSector == newSectorSize)
             return;
-        mRows = newRows;
-
-        mMaskMatrix.resize(mRows, std::vector<bool>(mColumns, false));
-        mTileSize.setHeight(height()/mRows);
-
-        emit rowsChanged();
-        updateMask();
-    }
-
-    void setColumns(int newColumns) {
-        if(mColumns == newColumns)
-            return;
-        mColumns = newColumns;
-
-        for(auto &row : mMaskMatrix)
-            row.resize(mColumns, false);
-        mTileSize.setWidth(width()/mColumns);
-
-        emit columnsChanged();
-        updateMask();
-    }
-
-    void setTransposed(TransposeType newTransposed) {
-        if(mTransposed == newTransposed)
-            return;
-        mTransposed = newTransposed;
-
-        emit transposedChanged();
-        updateMask();
-    }
-
-public slots:
-    void updateMask() {
-        QRegion region;
-        booleanMatrix mask = mMaskMatrix;
-
-        if(mTransposed != TransposeType::None) {
-            mask = transpose(mMaskMatrix, mTransposed == TransposeType::SecondaryTranspose);
-        }
-
-        /// * matrix row/col may change after transpose. (not in this program)
-        int row = mask.size(), column = mask[0].size();
-
-        for(int i = 0; i < row; ++i) {
-            for(int j = 0 ; j < column; ++j) {
-                if(mask[i][j] == true) {
-                    QPoint pos(j * mTileSize.width(), i * mTileSize.height());
-                    region = region.united(QRect(pos - QPoint(2, 2), mTileSize + QSize(4, 4)));
-                }
-            }
-        }
-
-        setMask(region);
+        mSector = newSectorSize;
+        emit sectorChanged();
         update();
     }
 
-    void setMaskArea(int i, int j, int i2, int j2, bool val = true) {
-        if(0 <= i && 0 <= j && i2 < mRows && j2 < mColumns) {
-            for(; i <= i2; ++i) {
-                for(int k = j; k <= j2; ++k) {
-                    mMaskMatrix[i][k] = val;
-                }
-            }
-            updateMask();
-        }
+public slots:
+    void update() {
+        updateMask();
+        QQuickWindow::update();
     }
 
-    void toggleMaskArea(int i, int j, int i2, int j2) {
-        if(0 <= i && 0 <= j && i2 < mRows && j2 < mColumns) {
-            for(; i < i2; ++i) {
-                for(; j < j2; ++j) {
-                    mMaskMatrix[i][j] = !mMaskMatrix[i][j];
-                }
-            }
-            updateMask();
-        }
-    }
+    void updateMask() {
+        if(mSector.isEmpty() || !isActive()) return;
 
-    booleanMatrix transpose(booleanMatrix matrix, bool secondaryDiagonal = true) {
-        if(mRows != mColumns)/// FIXME: handle mRow != mColumn.
-            return matrix;
-        for(int i = 0; i < mRows; ++i) {
-            for(int j = 0 ; j < mColumns; ++j) {
-                if(secondaryDiagonal == true && i + j < std::min(mRows, mColumns)) {
-                    std::vector<bool>::swap(matrix[mColumns - j - 1][mRows - i - 1], matrix[i][j]);
-                } else if(secondaryDiagonal == false && j < i) {
-                    std::vector<bool>::swap(matrix[j][i], matrix[i][j]);
+        QRegion region(width() - 28, 0, mSector.width(), mSector.height());
+        QImage windowImage = grabWindow().scaled(width(), height());
+
+        int sectorWidth = mSector.width(), sectorHeight = mSector.height();
+        int rows = width() / sectorWidth, cols = height() / sectorHeight;
+
+        for(int i = 0; i < rows; ++i) {
+            for(int j = 0 ; j < cols; ++j) {
+                QPoint topLeft(sectorWidth * i, sectorHeight * j);
+                QPoint center = (topLeft + QPoint(sectorWidth/2, sectorHeight/2));
+
+                if(qAlpha(windowImage.pixel(center.x(), center.y()))) {
+                    region = region.united(QRegion(QRect(topLeft, mSector)));
                 }
             }
         }
-        return matrix;
+        setMask(region);
     }
 
-    /**
-     * NOTE: transparent mouse & keyboard input on specific spots.
-     * https://stackoverflow.com/questions/20401896/qt-global-mouse-listener
-     * https://stllug.sluug.org/meeting_notes/1997/0619/xkey.html
-     * https://stackoverflow.com/questions/8629474/how-to-globally-capture-every-mouse-click-in-x11
-     */
 signals:
-    void rowsChanged();
-    void columnsChanged();
-    void transposedChanged();
+    void sectorChanged();
 
 private:
-    int mRows, mColumns;
-    QSize mTileSize;
-    TransposeType mTransposed;
-    booleanMatrix mMaskMatrix;
+    QSize mSector;
 };
 
 static void registerKPMaskWindowType() {
-    qmlRegisterType<tileMaskWindow>("knight.pen.window", 1, 0, "TileMaskWindow");
+    qmlRegisterType<maskWindow>("knight.pen.window", 1, 0, "MaskWindow");
 }
 Q_COREAPP_STARTUP_FUNCTION(registerKPMaskWindowType)
